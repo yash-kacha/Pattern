420d8e4b2813814ef25749b63351283ef6b9f028
Yash Kacha <yashkacha2705@gmail.com>
2025-02-13 22:16:47 +0530

    Update Main.java

--- AtoZ/Main.java
+++ AtoZ/Main.java
@@ -1,9 +1,13 @@
 import java.util.Scanner;
 
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
-        Logic logic = new Logic();
+
+
+        Logic2 logic = new Logic2();
 
 
         Scanner sc = new Scanner(System.in);
@@ -18,9 +22,9 @@ public class Main {
         System.out.print("Enter a Char : ");
         char ch = sc.next().charAt(0);
 
+        System.out.println("\n\n");
         logic.setCh(ch);
         logic.setSize(size);
-
 
         logic.logicOp(text.toUpperCase());
     }
